3a8f3047e9a739d938df51b0ac4a8dfba483181f
semi <semi1907@outlook.com>
2024-07-18 18:54:56 +0300

    Hashtable Uygulaması. Bu uygulama Hashtable kullanarak Stringleri adres için geçerli formata çevirir.

--- Program.cs
+++ Program.cs
@@ -12,71 +12,34 @@ namespace BtkAkademiVeriYapilariTemel
     {
         static void Main(string[] args)
         {
+            //Hashtable Örnek Proje 
+            Console.WriteLine("Başlığı Giriniz : ");
+            string baslik = Console.ReadLine();
+
+            //küçültme
+            baslik = baslik.ToLower();
+            //BTK Akademi'den Yeni Eğitim Aldım
             //Hashtable
-            //Declaration
-            var sehirler = new Hashtable();
-
-            //Add
-            sehirler.Add(17, "Çanakkale");
-            sehirler.Add(34, "Istanbul");
-            sehirler.Add(35, "Izmir");
-            sehirler.Add(23, "Elazığ");
-
-            //Travel
-            foreach (DictionaryEntry item in sehirler)
+            var karakterSeti = new Hashtable()
             {
-                Console.WriteLine(item.Key + " - " + item.Value);
+                {'ü','u'},
+                {'ı','i'},
+                {'ğ','g'},
+                {' ','-'},
+                {'\'','-'},
+                {'ş','s'},
+                {'ö','o'},
+                {'ç','c'},
+                {'.','-'},
+                {'?','-'}
+            };
+
+            foreach(DictionaryEntry item in karakterSeti)
+            {
+                baslik = baslik.Replace((char)item.Key, (char)item.Value);
             }
 
-            //Keys
-            var anahtarlar = sehirler;
-            Console.WriteLine("\nAnahtarlar: ");
-            foreach (DictionaryEntry item in sehirler)
-            {
-                Console.WriteLine(item.Key);
-            }
-
-            //Values
-
-            Console.WriteLine("\nDeğerler: ");
-
-            /*
-            
-            Hashtable degerler = sehirler;
-            foreach(DictionaryEntry item in degerler)            ===> Bu, bir yol. Bütün hash table'ı alıp foreach'de values seçmek.
-            {
-                Console.WriteLine(item.Value);
-            }
-            
-            */
-
-            /*
-             
-            var degerler = sehirler.Values;
-            foreach (var item in degerler)                       ===> Bu, bir yol. Bütün var sayesinde Hashtable'ın sadece values kısmını almak. 
-            {                                                   *Bir hashtable'a başka bir Hashtable'ın sadece değerlerini atamazsınç Çünkü Key değerleri de olmak zorunda.
-                Console.WriteLine(item);
-            }
-
-            */
-
-            ICollection degerler = sehirler.Values;
-            foreach(var item in degerler)
-            {
-                Console.WriteLine(item);                        //Bu da olur. Neden ICollection interface'ini data type olarak kullandığımızı ileride çok daha iyi anlayacağız.
-            }                                                   //Ama sanırım List<T>, Hashtable, ArrayList vs.. gibi Collection'ları aynı arayüzle yönetebilmemize yarıyor. 
-
-            //Reach an element
-            Console.WriteLine("\nElemana Erişme :");
-            Console.WriteLine(sehirler[34]);
-
-            //Delete an element
-            Console.WriteLine("\nEleman Silme :");
-            sehirler.Remove(23);
-            foreach(DictionaryEntry item in sehirler)
-            {
-                Console.WriteLine(item.Key + " - " + item.Value);
-            }
+            Console.WriteLine(baslik);
 
             Console.ReadKey();
         }
