4b95605b3fcfc6104f2b794b8b0cec8726ee4123
semi <semi1907@outlook.com>
2024-07-18 12:34:13 +0300

    Struct oluşturuldu, ctrl .  ile Program.cs içerisinde ayrı bir dosyada tutuluyor.

---
+++ Ogrenci.cs
@@ -0,0 +1,30 @@
+﻿namespace BtkAkademiVeriYapilariTemel
+{
+    internal partial class Program
+    {
+        public struct Ogrenci
+        {
+            private int ogr_no { get; set; }
+            private string ogr_isim { get; set; }
+            private string ogr_soyisim { get; set; }
+            private bool ogr_cinsiyet { get; set; }
+
+            public Ogrenci(int ogr_no, string ogr_isim, string ogr_soyisim, bool ogr_cinsiyet)
+            {
+                this.ogr_no = ogr_no;
+                this.ogr_isim = ogr_isim;
+                this.ogr_soyisim = ogr_soyisim;
+                this.ogr_cinsiyet = ogr_cinsiyet;
+            }
+
+            public override string ToString()
+            {
+                return $"İsim : {this.ogr_isim},20" +
+                    $"Soyisim : {this.ogr_soyisim}" +
+                    $"Cinsiyet : {this.ogr_cinsiyet}" +
+                    $"Numara : {this.ogr_no}";
+            }
+
+        }
+    }
+}
