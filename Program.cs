361d39bbe84f1c25d9ca92462dc5f1a648a2b79e
semi <semi1907@outlook.com>
2024-07-19 20:04:48 +0300

    Linked List Fonksiyonlarla Basit Kullanımı

--- Program.cs
+++ Program.cs
@@ -16,42 +16,41 @@ namespace BtkAkademiVeriYapilariTemel
     {
         static void Main(string[] args)
         {
-            var sesliHarfler = new List<char>()
+            var sehirler = new LinkedList<string>();
+            sehirler.AddFirst("Ordu");
+            sehirler.AddFirst("Trabzon");
+            sehirler.AddLast("Istanbul");
+            
+            sehirler.AddAfter(sehirler.Find("Ordu"), "Samsun");
+            sehirler.AddBefore(sehirler.First.Next.Next, "Giresun");
+            sehirler.AddAfter(sehirler.Last.Previous, "Sinop");
+            sehirler.AddAfter(sehirler.Find("Sinop"), "Zonguldak");
+                       
+            foreach(string s in sehirler)
             {
-                {'e'}, {'u'}, {'ı'}, {'o'}, {'ü'}, {'a'}, {'i'}, {'ö'}
-            };
-
-            ConsoleKeyInfo secim;
-            var queue1 = new Queue<char>();
-
-            foreach (char c in sesliHarfler)
-            {
-                Console.WriteLine($"{c} kuyruğa eklensin mi? [e/h]");
-                secim = Console.ReadKey();
-                if (secim.Key == ConsoleKey.E)
-                {
-                    queue1.Enqueue(c);
-                    Console.WriteLine($"\n{c} kuyruğa eklendi.");
-                    Console.WriteLine($"Kuyruktaki Eleman Sayısı : {queue1.Count}");
-                }
+                Console.WriteLine(s);
             }
 
-            Console.WriteLine();
-            Console.WriteLine("Kuyruktaki elemanları silme işlemi için ESC tuşuna basınız");
-            Console.WriteLine();
-            secim = Console.ReadKey();
+            Console.WriteLine("\nGİDİŞ GÜZERGAHI :\n");
 
-            if (secim.Key == ConsoleKey.Escape)
+            var gecici = sehirler.First;
+            while (gecici != null)
             {
-                while (queue1.Count > 0)
-                {
-                    Console.WriteLine($"{queue1.Peek()}, Kuyruktan Çıkartılacak.");
-                    Console.WriteLine($"{queue1.Dequeue()}, Kuyruktan Çıkartıldı.");
-                    Console.WriteLine($"Kuyruktaki Eleman Sayısı : {queue1.Count}");
-                }
-
-                Console.ReadKey();
+                Console.WriteLine(gecici.Value);
+                gecici = gecici.Next;
             }
+
+            Console.WriteLine("\nDÖNÜŞ GÜZERGAHI :\n");
+
+            var gecici2 = sehirler.Last;
+            while (gecici2 != null)
+            {
+                Console.WriteLine(gecici2.Value);
+                gecici2 = gecici2.Previous;
+            }
+
+            Console.ReadKey();
+            
         }
     }
 }
