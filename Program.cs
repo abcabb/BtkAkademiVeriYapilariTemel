6e080b99a1570b1a8f512798383b670fc4ab1408
semi <semi1907@outlook.com>
2024-07-19 15:54:37 +0300

    Stack Uygulama

--- Program.cs
+++ Program.cs
@@ -13,6 +13,28 @@ namespace BtkAkademiVeriYapilariTemel
     {
         static void Main(string[] args)
         {
+            var karakterStack = new Stack<char>();
+
+            for(int i = 65; i<=90; i++)
+            {
+                karakterStack.Push((char)i);
+                Console.WriteLine($"{karakterStack.Peek()}, Yığına Eklendi.");
+                Console.WriteLine("Yığındaki Eleman Sayısı = {0}",karakterStack.Count);
+            }
+            Console.WriteLine("Press a button to clear the Stack :");
+            Console.ReadKey();
+
+            while (karakterStack.Count > 0)
+            {
+                Console.WriteLine($"{karakterStack.Pop()}, Yığından Çıkarıldı.");
+                Console.WriteLine("Yığındaki Eleman Sayısı = {0}", karakterStack.Count);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void StackOrnegi()
+        {
             //Stack Declaration
             var stack1 = new Stack<char>();
 
@@ -28,9 +50,6 @@ namespace BtkAkademiVeriYapilariTemel
             Console.WriteLine("Yığından çıkartılan eleman : {0}", stack1.Pop());
             Console.WriteLine("Yığından çıkartılan eleman : {0}", stack1.Pop());
             Console.WriteLine("Yığından çıkartılan eleman : {0}", stack1.Pop());
-
-
-            Console.ReadKey();
         }
     }
 }
