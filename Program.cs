86ca7ea47c5ad3f8ae192fad8ce64e62e4afdbc4
semi <semi1907@outlook.com>
2024-07-19 10:53:31 +0300

    Sorted List Bütün Temel Uygulamaları

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -12,34 +13,68 @@ namespace BtkAkademiVeriYapilariTemel
     {
         static void Main(string[] args)
         {
-            //Hashtable Örnek Proje 
-            Console.WriteLine("Başlığı Giriniz : ");
-            string baslik = Console.ReadLine();
+            //Sorted List
 
-            //küçültme
-            baslik = baslik.ToLower();
-            //BTK Akademi'den Yeni Eğitim Aldım
-            //Hashtable
-            var karakterSeti = new Hashtable()
+            var liste = new SortedList()
             {
-                {'ü','u'},
-                {'ı','i'},
-                {'ğ','g'},
-                {' ','-'},
-                {'\'','-'},
-                {'ş','s'},
-                {'ö','o'},
-                {'ç','c'},
-                {'.','-'},
-                {'?','-'}
+                {1, "Bir"},
+                {2, "İki"},
+                {7, "Yedi"},
+                {3, "Üç"},
+                {6, "Altı"},
+                {9, "Dokuz"},
             };
+            
+            liste.Add(4, "Dört");
 
-            foreach(DictionaryEntry item in karakterSeti)
+            foreach(DictionaryEntry item in liste)
             {
-                baslik = baslik.Replace((char)item.Key, (char)item.Value);
+                Console.WriteLine("{0} :       {1}", item.Key, item.Value);
+            }
+            //Elemanların Sıralanmış olarak listeye eklendiğini görüyoruz.
+
+            Console.WriteLine("Listedeki eleman sayısı :    {0}",liste.Count);
+            Console.WriteLine("Listenin kapasitesi :       {0}", liste.Capacity);
+            liste.TrimToSize();
+            Console.WriteLine("Listenin kapasitesi :     {0}", liste.Capacity);
+
+            //Key ile Value değerine erişme
+            Console.WriteLine(liste[3]);
+
+            //Index ile Value değerine erişme
+            Console.WriteLine(liste.GetByIndex(0));
+
+            //Index ile Key değerine erişme
+            Console.WriteLine(liste.GetKey(0));
+
+            //Liste sonundaki elemanın Value değeri
+            Console.WriteLine(liste.GetByIndex(liste.Count-1));
+
+            //Liste sonundaki elemanın anahtarını almak
+            Console.WriteLine(liste.GetKey(liste.Count-1));
+
+            //Listenin Anahtarlarını gösterme
+            Console.WriteLine("ANAHTARLAR");
+            var anahtarlar = liste.Keys;
+            foreach(var a in anahtarlar)
+            {
+                Console.WriteLine(a);
             }
 
-            Console.WriteLine(baslik);
+            //Listenin Değerlerini gösterme
+            Console.WriteLine("DEĞERLER");
+            var degerler = liste.Values;
+            foreach(var d in degerler)
+            {
+                Console.WriteLine(d);
+            }
+
+            if (liste.ContainsKey(3))
+            {
+                liste[3] = "Three";
+            }
+
+            foreach(DictionaryEntry i in liste) {  Console.WriteLine(i.Key + " - " + i.Value); }
 
             Console.ReadKey();
         }
