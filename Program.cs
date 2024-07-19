023c35e541f52ce0e2f0a6ef4bc5aafce7f4937a
semi <semi1907@outlook.com>
2024-07-19 23:13:55 +0300

    SortedSet Kümeler Uygulamaları

--- Program.cs
+++ Program.cs
@@ -17,27 +17,39 @@ namespace BtkAkademiVeriYapilariTemel
     {
         static void Main(string[] args)
         {
-            var list = new List<int>();
-            var r = new Random();
+            //SortedSet KÜMELER Uygulaması
 
-            for (int i = 0; i<1000; i++)
-            {
-                list.Add(r.Next(0, 20));
-                Console.Write($"{list[i]}  ");
-            }
+            var A = new SortedSet<int>(RastgeleSayiUret(100)); //SortedSet'in Constructor'larından bir tanesi args olarak Collection aldığı için List<int>'i const'a arg olarak girebildik.        
+            var B = new SortedSet<int>(RastgeleSayiUret(10));
+           
+            Console.WriteLine("A Kümesi :");
+            foreach(int i in A) Console.Write($"{i} ");
 
-            var set = new SortedSet<int>(list);
+            Console.WriteLine("\nB Kümesi :");
+            foreach(int i in B) Console.Write($"{i} ");
 
-            Console.WriteLine("\n\nUnique(Benzersiz) Elemanlar :\n");
+            //A.UnionWith(B); // A ve B kümesinin birleşimi
+            //A.IntersectWith(B); // A ve B kümesinin kesişimi
+            //A.ExceptWith(B); // A'dan B kümesi Çıkarsa 
+            A.SymmetricExceptWith(B); // Kesişim dışındaki elemanların birleşimi
+            Console.WriteLine("\nA ve B'nin Kesişim Bölgesinde Olmayan Elemanlar :");  //Sorted set Reference Type'lıdır !!!!
+            foreach(int i in A) Console.Write($"{i} ");
+            Console.WriteLine("\nToplam sayısı : {0}", A.Count);
 
-            foreach(int i in set)
-            {
-                Console.Write("{0} ", i);
-            }
-
-            Console.WriteLine($"\n{set.Count} tane benzersiz eleman vardır");
 
             Console.ReadKey();
+        }
+
+        static List<int> RastgeleSayiUret(int n)
+        {
+            var list = new List<int>();
+            var r = new Random();
+            for(int i = 0; i < n; i++)
+            {
+                list.Add(r.Next(1,1000));
+            }
+
+            return list;
         }
     }
 }
