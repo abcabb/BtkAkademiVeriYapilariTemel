c07bc6a217a5c57c67a09f3230c71a11f2560e2f
semi <semi1907@outlook.com>
2024-07-19 14:37:55 +0300

    Generic olan Class'lara ICollection ve IEnumeration Interface'lerinin Implementasyonu

--- Program.cs
+++ Program.cs
@@ -13,21 +13,7 @@ namespace BtkAkademiVeriYapilariTemel
     {
         static void Main(string[] args)
         {
-            var kitapIcerigi = new SortedList()
-            {
-                {1, "Giriş"},
-                {25, "Değişkenler"},
-                {40, "Operatörler"},
-                {50, "Döngüler"},
-                {80, "Fonksiyonlar"}
-            };
-            Console.WriteLine("KİTAP İÇERİĞİ\n" + new string('-',50));
-            Console.WriteLine($"{"Konu Başlığı", -20}{"Sayfa Sayısı", -5}");
-            foreach(DictionaryEntry listeElemanı in kitapIcerigi)
-            {
-                Console.WriteLine($"{listeElemanı.Value, -20}{listeElemanı.Key,-5}");
-            }
-
+            
             Console.ReadKey();
         }
     }
